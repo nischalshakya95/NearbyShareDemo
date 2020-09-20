28c9c365b3a7f23f1c63a1385057a6e4659855b9
nischalshakya95 <nischalshakya@lftechnology.com>
2020-09-20 21:48:37 +0545

    Fixed alert dialog context

--- app/src/main/java/com/dinube/nearbysharedemo/MainActivity.java
+++ app/src/main/java/com/dinube/nearbysharedemo/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        context = getApplicationContext();
+        context = this;
         endpointName = Build.MODEL;
         recyclerView = findViewById(R.id.endpointsRecyclerView);
 
