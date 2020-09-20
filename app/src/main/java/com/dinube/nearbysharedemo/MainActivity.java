3db117e2cad4c01648a17449b8bddf035952205b
nischalshakya95 <nischalshakya@lftechnology.com>
2020-09-20 13:32:35 +0545

    Display the list of endpoint to connect to

---
+++ app/src/main/java/com/dinube/nearbysharedemo/MainActivity.java
@@ -0,0 +1,172 @@
+package com.dinube.nearbysharedemo;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.nearby.Nearby;
+import com.google.android.gms.nearby.connection.AdvertisingOptions;
+import com.google.android.gms.nearby.connection.ConnectionInfo;
+import com.google.android.gms.nearby.connection.ConnectionLifecycleCallback;
+import com.google.android.gms.nearby.connection.ConnectionResolution;
+import com.google.android.gms.nearby.connection.ConnectionsStatusCodes;
+import com.google.android.gms.nearby.connection.DiscoveredEndpointInfo;
+import com.google.android.gms.nearby.connection.DiscoveryOptions;
+import com.google.android.gms.nearby.connection.EndpointDiscoveryCallback;
+import com.google.android.gms.nearby.connection.Payload;
+import com.google.android.gms.nearby.connection.PayloadCallback;
+import com.google.android.gms.nearby.connection.PayloadTransferUpdate;
+import com.google.android.gms.nearby.connection.Strategy;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Context context;
+
+    private String endpointName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        context = getApplicationContext();
+        endpointName = Build.MODEL;
+
+        Button startAdvertising = findViewById(R.id.advertise);
+        startAdvertising.setOnClickListener(click -> startAdvertising());
+
+        Button stopAdvertising = findViewById(R.id.stopAdversiting);
+        stopAdvertising.setOnClickListener(click -> stopAdvertising());
+
+        Button startDiscovering = findViewById(R.id.discover);
+        startDiscovering.setOnClickListener(click -> startDiscovering());
+
+        Button stopDiscovering = findViewById(R.id.stopDiscovering);
+        stopDiscovering.setOnClickListener(click -> stopDiscovering());
+    }
+
+    private final EndpointDiscoveryCallback endpointDiscoveryCallback = new EndpointDiscoveryCallback() {
+        @Override
+        public void onEndpointFound(@NonNull String s, @NonNull DiscoveredEndpointInfo discoveredEndpointInfo) {
+            Nearby.getConnectionsClient(context).requestConnection(endpointName, s, connectionLifecycleCallback)
+                    .addOnSuccessListener((Void unused) -> {
+                        Toast.makeText(context,
+                                "We successfully request the connection now both side must accept before the connection is established",
+                                Toast.LENGTH_SHORT)
+                                .show();
+                    })
+                    .addOnFailureListener((Exception e) -> {
+                        showToast("Exception in endpoint discovery callback ", e.getMessage());
+                    });
+        }
+
+        @Override
+        public void onEndpointLost(@NonNull String s) {
+            showToast("On endpoint lost ", s);
+        }
+    };
+
+    private final ConnectionLifecycleCallback connectionLifecycleCallback = new ConnectionLifecycleCallback() {
+        @Override
+        public void onConnectionInitiated(@NonNull String endpointId, @NonNull ConnectionInfo connectionInfo) {
+            showToast("Endpoints ", connectionInfo.getEndpointName());
+            new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("Accept connection to " + connectionInfo.getEndpointName())
+                    .setMessage("Confirm the code matches on both devices: " + connectionInfo.getAuthenticationToken())
+                    .setPositiveButton(
+                            "Accept",
+                            (DialogInterface dialog, int which) ->
+                                    // The user confirmed, so we can accept the connection.
+                                    Nearby.getConnectionsClient(context)
+                                            .acceptConnection(endpointId, payloadCallback))
+                    .setNegativeButton(
+                            android.R.string.cancel,
+                            (DialogInterface dialog, int which) ->
+                                    // The user canceled, so we should reject the connection.
+                                    Nearby.getConnectionsClient(context).rejectConnection(endpointId))
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
+        }
+
+        @Override
+        public void onConnectionResult(@NonNull String s, @NonNull ConnectionResolution connectionResolution) {
+            switch (connectionResolution.getStatus().getStatusCode()) {
+                case ConnectionsStatusCodes.STATUS_OK:
+                    Toast.makeText(context, "We're connected! Can now start sending and receiving data", Toast.LENGTH_SHORT).show();
+                    break;
+                case ConnectionsStatusCodes.STATUS_CONNECTION_REJECTED:
+                    Toast.makeText(context, "Connection rejected", Toast.LENGTH_SHORT).show();
+                    break;
+                case ConnectionsStatusCodes.STATUS_ERROR:
+                    Toast.makeText(context, "Connection status error", Toast.LENGTH_SHORT).show();
+                    break;
+                default:
+                    Toast.makeText(context, "Unknown status code", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        @Override
+        public void onDisconnected(@NonNull String s) {
+            Toast.makeText(context, "We've been disconnected from the endpoint. No more data can be send or received.", Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    private PayloadCallback payloadCallback =
+            new PayloadCallback() {
+                @Override
+                public void onPayloadReceived(@NonNull String endpointId, Payload payload) {
+                    // This always gets the full data of the payload. Will be null if it's not a BYTES
+                    // payload. You can check the payload type with payload.getType().
+                    byte[] receivedBytes = payload.asBytes();
+                }
+
+                @Override
+                public void onPayloadTransferUpdate(@NonNull String endpointId, @NonNull PayloadTransferUpdate update) {
+                    // Bytes payloads are sent as a single chunk, so you'll receive a SUCCESS update immediately
+                    // after the call to onPayloadReceived().
+                }
+            };
+
+    private void startAdvertising() {
+        AdvertisingOptions advertisingOptions = new AdvertisingOptions.Builder().setStrategy(Strategy.P2P_CLUSTER).build();
+        Nearby.getConnectionsClient(context).startAdvertising(endpointName, "com.dinube.nearbysharedemo", connectionLifecycleCallback, advertisingOptions)
+                .addOnSuccessListener((Void unused) -> {
+                    showToast("Advertising ", endpointName);
+                })
+                .addOnFailureListener((e) -> {
+                    showToast("Exception in start advertising ", e.getMessage());
+                });
+    }
+
+    private void startDiscovering() {
+        DiscoveryOptions discoveryOptions = new DiscoveryOptions.Builder().setStrategy(Strategy.P2P_CLUSTER).build();
+        Nearby.getConnectionsClient(context).startDiscovery("com.dinube.nearbysharedemo", endpointDiscoveryCallback, discoveryOptions)
+                .addOnSuccessListener((Void unused) -> {
+                    showToast("Discovering from ", endpointName);
+                })
+                .addOnFailureListener((e) -> {
+                    showToast("Exception in discovering ", e.getMessage());
+                });
+    }
+
+    private void stopAdvertising() {
+        Nearby.getConnectionsClient(context).stopAdvertising();
+        showToast("Advertise Stopped ", endpointName);
+    }
+
+    private void stopDiscovering() {
+        Nearby.getConnectionsClient(context).stopDiscovery();
+        showToast("Discovering Stopped ", endpointName);
+    }
+
+    private void showToast(String s, String endpointName) {
+        Toast.makeText(context, s + endpointName, Toast.LENGTH_SHORT).show();
+    }
+}
