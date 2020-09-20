e17b7e31534b41e66993de92865c65068f0ea95a
nischalshakya95 <nischalshakya@lftechnology.com>
2020-09-20 21:43:06 +0545

    Optimize code of nearbyshare

--- app/src/main/java/com/dinube/nearbysharedemo/MainActivity.java
+++ app/src/main/java/com/dinube/nearbysharedemo/MainActivity.java
@@ -5,22 +5,12 @@ import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
 import android.widget.Switch;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.dinube.nearbysharedemo.fragment.EndpointsListAdapter;
-import com.dinube.nearbysharedemo.nearby.NearbyConnectionLifeCycleCallback;
-import com.dinube.nearbysharedemo.nearby.NearbyEndPointDiscoveryCallback;
-import com.google.android.gms.nearby.Nearby;
-import com.google.android.gms.nearby.connection.AdvertisingOptions;
-import com.google.android.gms.nearby.connection.DiscoveryOptions;
-import com.google.android.gms.nearby.connection.Strategy;
-
-import java.util.ArrayList;
-import java.util.List;
+import com.dinube.nearbysharedemo.nearby.NearbyAdvertise;
+import com.dinube.nearbysharedemo.nearby.NearbyDiscover;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,8 +19,6 @@ public class MainActivity extends AppCompatActivity {
     private String endpointName;
 
     private RecyclerView recyclerView;
-
-    private final List<String> endpoints = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,11 +33,9 @@ public class MainActivity extends AppCompatActivity {
         Switch advertiseSwitch = findViewById(R.id.advertiseSwitch);
         advertiseSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
             if (isChecked) {
-                startAdvertising();
+                NearbyAdvertise.startAdvertising(context, endpointName, recyclerView);
             } else {
-                stopAdvertising();
-                endpoints.clear();
-                generateDataList(endpoints);
+                NearbyAdvertise.stopAdvertising(context, endpointName);
             }
         });
 
@@ -57,57 +43,10 @@ public class MainActivity extends AppCompatActivity {
         Switch discoverSwitch = findViewById(R.id.discoverSwitch);
         discoverSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
             if (isChecked) {
-                startDiscovering();
+                NearbyDiscover.startDiscovering(context, endpointName, recyclerView);
             } else {
-                stopDiscovering();
-                endpoints.clear();
-                generateDataList(endpoints);
+                NearbyDiscover.stopDiscovering(context, endpointName);
             }
         });
-    }
-
-    private void generateDataList(List<String> endpoints) {
-        recyclerView = findViewById(R.id.endpointsRecyclerView);
-        EndpointsListAdapter adapter = new EndpointsListAdapter(context, endpoints);
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
-        recyclerView.setLayoutManager(layoutManager);
-        recyclerView.setAdapter(adapter);
-    }
-
-    private void startAdvertising() {
-        AdvertisingOptions advertisingOptions = new AdvertisingOptions.Builder().setStrategy(Strategy.P2P_CLUSTER).build();
-        Nearby.getConnectionsClient(context).startAdvertising(endpointName, "com.dinube.nearbysharedemo", new NearbyConnectionLifeCycleCallback(context, recyclerView), advertisingOptions)
-                .addOnSuccessListener((Void unused) -> {
-                    showToast("Advertising ", endpointName);
-                })
-                .addOnFailureListener((e) -> {
-                    showToast("Exception in start advertising ", e.getMessage());
-                });
-    }
-
-    private void startDiscovering() {
-        DiscoveryOptions discoveryOptions = new DiscoveryOptions.Builder().setStrategy(Strategy.P2P_CLUSTER).build();
-        Nearby.getConnectionsClient(context).startDiscovery("com.dinube.nearbysharedemo",
-                new NearbyEndPointDiscoveryCallback(context, endpointName, new NearbyConnectionLifeCycleCallback(context, recyclerView)), discoveryOptions)
-                .addOnSuccessListener((Void unused) -> {
-                    showToast("Discovering from ", endpointName);
-                })
-                .addOnFailureListener((e) -> {
-                    showToast("Exception in discovering ", e.getMessage());
-                });
-    }
-
-    private void stopAdvertising() {
-        Nearby.getConnectionsClient(context).stopAdvertising();
-        showToast("Advertise Stopped ", endpointName);
-    }
-
-    private void stopDiscovering() {
-        Nearby.getConnectionsClient(context).stopDiscovery();
-        showToast("Discovering Stopped ", endpointName);
-    }
-
-    private void showToast(String s, String endpointName) {
-        Toast.makeText(context, s + endpointName, Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/com/dinube/nearbysharedemo/nearby/NearbyDiscover.java
@@ -0,0 +1,30 @@
+package com.dinube.nearbysharedemo.nearby;
+
+import android.content.Context;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.dinube.nearbysharedemo.utils.UiUtils;
+import com.google.android.gms.nearby.Nearby;
+import com.google.android.gms.nearby.connection.DiscoveryOptions;
+import com.google.android.gms.nearby.connection.Strategy;
+
+public class NearbyDiscover {
+
+    public static void startDiscovering(Context context, String endpointName, RecyclerView recyclerView) {
+        DiscoveryOptions discoveryOptions = new DiscoveryOptions.Builder().setStrategy(Strategy.P2P_CLUSTER).build();
+        Nearby.getConnectionsClient(context).startDiscovery("com.dinube.nearbysharedemo",
+                new NearbyEndPointDiscoveryCallback(context, endpointName, new NearbyConnectionLifeCycleCallback(context, recyclerView)), discoveryOptions)
+                .addOnSuccessListener((Void unused) -> {
+                    UiUtils.showToast(context, "Discovering from ", endpointName);
+                })
+                .addOnFailureListener((e) -> {
+                    UiUtils.showToast(context, "Exception in discovering ", e.getMessage());
+                });
+    }
+
+    public static void stopDiscovering(Context context, String endpointName) {
+        Nearby.getConnectionsClient(context).stopDiscovery();
+        UiUtils.showToast(context, "Discovering Stopped ", endpointName);
+    }
+}
